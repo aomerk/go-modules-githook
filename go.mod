b603f3516ea2c12f801abce2e1dc49c75ccf1cfa
aomerk <aliomerkeser@gmail.com>
2021-02-13 16:26:58 +0300

    use fasthttp and grpc as examples

--- go.mod
+++ go.mod
@@ -1,3 +1,12 @@
 module github.com/aomerk/go-modules-githook
 
 go 1.15
+
+replace github.com/valyala/fasthttp => ./vendor/blah/fasthttp
+
+require (
+	github.com/valyala/fasthttp v1.20.0
+	google.golang.org/grpc v1.35.0
+)
+
+replace google.golang.org/grpc => google.golang.org/grpc v1.26.0
--- main.go
+++ main.go
@@ -1,29 +1,57 @@
 package main
 
 import (
+	"flag"
 	"fmt"
-	"net/http"
+	"log"
+
+	"github.com/valyala/fasthttp"
 )
 
-func handler(writer http.ResponseWriter, request *http.Request) {
-	// i want to use my local fmt package tho
-	_, err := fmt.Fprintf(writer, "blorg")
-	if err != nil {
-		panic(err.Error())
+// taken from fasthttp examples
+
+var (
+	addr     = flag.String("addr", ":8080", "TCP address to listen to")
+	compress = flag.Bool("compress", false, "Whether to enable transparent response compression")
+)
+
+func main() {
+	flag.Parse()
+
+	h := requestHandler
+	if *compress {
+		h = fasthttp.CompressHandler(h)
+	}
+
+	if err := fasthttp.ListenAndServe(*addr, h); err != nil {
+		log.Fatalf("Error in ListenAndServe: %s", err)
 	}
 }
 
-func main() {
-	var (
-		err  error
-		addr string
-	)
-	addr = ":9999"
-	http.HandleFunc("/", handler)
+func requestHandler(ctx *fasthttp.RequestCtx) {
+	fmt.Fprintf(ctx, "Hello, world!\n\n")
 
-	err = http.ListenAndServe(addr, nil)
-	if err != nil {
-		// this'll never be nil tho
-		panic(err.Error())
-	}
+	fmt.Fprintf(ctx, "Request method is %q\n", ctx.Method())
+	fmt.Fprintf(ctx, "RequestURI is %q\n", ctx.RequestURI())
+	fmt.Fprintf(ctx, "Requested path is %q\n", ctx.Path())
+	fmt.Fprintf(ctx, "Host is %q\n", ctx.Host())
+	fmt.Fprintf(ctx, "Query string is %q\n", ctx.QueryArgs())
+	fmt.Fprintf(ctx, "User-Agent is %q\n", ctx.UserAgent())
+	fmt.Fprintf(ctx, "Connection has been established at %s\n", ctx.ConnTime())
+	fmt.Fprintf(ctx, "Request has been started at %s\n", ctx.Time())
+	fmt.Fprintf(ctx, "Serial request number for the current connection is %d\n", ctx.ConnRequestNum())
+	fmt.Fprintf(ctx, "Your ip is %q\n\n", ctx.RemoteIP())
+
+	fmt.Fprintf(ctx, "Raw request is:\n---CUT---\n%s\n---CUT---", &ctx.Request)
+
+	ctx.SetContentType("text/plain; charset=utf8")
+
+	// Set arbitrary headers
+	ctx.Response.Header.Set("X-My-Header", "my-header-value")
+
+	// Set cookies
+	var c fasthttp.Cookie
+	c.SetKey("cookie-name")
+	c.SetValue("cookie-value")
+	ctx.Response.Header.SetCookie(&c)
 }
